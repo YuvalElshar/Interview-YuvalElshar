25e75a6a297c2ccb9aa4ba34d862355a622be5c3
YuvalElshar <103168365+YuvalElshar@users.noreply.github.com>
2022-06-14 10:47:43 +0300

    Update cs5.cs

--- cs5.cs
+++ cs5.cs
@@ -2,7 +2,7 @@
 
 // The program prints 10 10 ... 10, line by line 10 times. The variable i that is added to each action in the list
 // is a reference and not value copy. That is why for every call it points to the same memory location and gets the same value. 
-// If you init a new value each time(like i did with j in line 18), a new address will be passed to the list,
+// If you init a new value each time(like i did with j), a new address will be passed to the list,
 // and every action will have it's own variable.
 using System;
 using System.Collections.Generic;
